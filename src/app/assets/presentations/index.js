c9ce781666ccc825bb80e395e4eac5ff1cc490ef
makecs50 <makecs50x@gmail.com>
2019-11-21 15:10:28 +0300

    Lecture4 downloading

--- src/app/assets/presentations/index.js
+++ src/app/assets/presentations/index.js
@@ -1,7 +1,8 @@
 import lecture1 from './Lecture1.zip'
 import lecture2 from './Lecture2.zip'
 import lecture3 from './Lecture3.zip'
+import lecture4 from './Lecture4.zip'
 
 export default {
-  lecture1, lecture2, lecture3
+  lecture1, lecture2, lecture3, lecture4
 }
