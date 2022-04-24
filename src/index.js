c98c080d0f2184adb3d7d49300a30163776fd00e
Lema <lex200900@gmail.com>
2022-04-24 23:31:40 +0300

    Chore: change to hashRouting

--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@ import './app/styles/index.css';
 import './app/styles/tailwind.css'
 import './app/assets/css/style.min.css';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
 
 const container = document.getElementById('root');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
-root.render(<BrowserRouter>
+root.render(<HashRouter>
   <App/>
-</BrowserRouter>);
+</HashRouter>);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
